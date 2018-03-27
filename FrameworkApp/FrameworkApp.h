5bfd824f9a6fe6cb891f1229d565fc726829f23a
Arunkumar <parunk14@ford.com>
2018-03-27 13:53:35 +0530

    Iniital commit

---
+++ FrameworkApp/FrameworkApp.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkApp.h
+//  FrameworkApp
+//
+//  Created by fordlabs on 3/27/18.
+//  Copyright © 2018 cognitors. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkApp.
+FOUNDATION_EXPORT double FrameworkAppVersionNumber;
+
+//! Project version string for FrameworkApp.
+FOUNDATION_EXPORT const unsigned char FrameworkAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkApp/PublicHeader.h>
+
+
---
+++ FrameworkApp/FrameworkClass.swift
@@ -0,0 +1,16 @@
+//
+//  FrameworkClass.swift
+//  FrameworkApp
+//
+//  Created by fordlabs on 3/27/18.
+//  Copyright © 2018 cognitors. All rights reserved.
+//
+
+import Foundation
+
+public class FrameworkClass {
+    
+    func printME(_ name:String) {
+        print("Wow \(name)")
+    }
+}
